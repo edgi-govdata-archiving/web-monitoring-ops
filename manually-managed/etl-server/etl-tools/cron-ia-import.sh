585e9abe2d85964cc5b638a713576a6486515b52
Rob Brackett <rob@robbrackett.com>
2020-04-19 22:14:15 -0700

    Use unplaybackable cache for imports
    We've had this capability for a while and I thought we had it turned on in production, but apparently not! Should help speed up imports.

--- manually-managed/etl-server/etl-tools/cron-ia-import.sh
+++ manually-managed/etl-server/etl-tools/cron-ia-import.sh
@@ -7,6 +7,7 @@ source /opt/conda/etc/profile.d/conda.sh
 conda activate web-monitoring-etl
 
 export LOG_LEVEL=INFO
+UNPLAYBACKABLE_CACHE="$HOME/etl-tools/unplaybackable.json"
 # --from is in hours:
 #  72 = 3 days
 # 120 = 5 days
@@ -14,5 +15,5 @@ export LOG_LEVEL=INFO
 # We typically keep this at 3-5 days to accomodate for any slow indexing
 # on Wayback's part.
 # They're experiencing major issues right now, so it's set higher.
-wm import ia-known-pages --from 192 --parallel 10 1>&2
+wm import ia-known-pages --from 192 --parallel 10 --unplaybackable "$UNPLAYBACKABLE_CACHE" 1>&2
 echo "Internet Archive import completed at `date`" 1>&2
